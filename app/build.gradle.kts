1d450087359ccbf9f69cbb2d74c2897c8670b7fe
aaronrojas32 <56888910+aaronrojas32@users.noreply.github.com>
2025-02-22 01:25:44 +0100

    Release v0.3.26-beta setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.6"
+        versionName = "0.3.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
