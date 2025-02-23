51b91ab1a547eaec25486e7df685875dce48479f
aaronrojas32 <56888910+aaronrojas32@users.noreply.github.com>
2025-02-23 13:45:59 +0100

    UI improvement and version 0.4.6-beta setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.3.26"
+        versionName = "0.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
